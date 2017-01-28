4cd374effe8a28df990770a9882b1eb1abf66ebe
MattPorto <matheus.pereira11@gmail.com>
2017-01-28 18:25:08 -0200

    set model tests

--- app/models/searched_term.rb
+++ app/models/searched_term.rb
@@ -1,18 +1,12 @@
 class SearchedTerm < ApplicationRecord
-  # attr_accessor :popularity, :term
-  validates :term, uniqueness: true
+
+  validates :term, presence: true
 
   def self.terms_for(prefix)
     Rails.cache.fetch(["search-terms", prefix]) do
       suggestions = where("term like ?", "#{prefix}_%")
       suggestions.order("popularity desc").limit(10).pluck(:term)
     end
-  end
-
-  def self.index_term(term)
-      where(term: term.downcase).first_or_initialize.tap do |suggestion|
-        suggestion.increment! :popularity
-      end
   end
 
 end
--- spec/models/searched_term_spec.rb
+++ spec/models/searched_term_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe SearchedTerm, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:term) }
+  end
+  context 'table fields' do
+    it { is_expected.to have_db_column(:term).of_type(:string) }
+    it { is_expected.to have_db_column(:popularity).of_type(:integer) }
+  end
 end
