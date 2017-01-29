73fd13f3a568945bdea70118a142b4b403077759
MattPorto <matheus.pereira11@gmail.com>
2017-01-29 03:49:57 -0200

    change most searched terms route name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "searched_terms#home"
   resources :searched_terms
-  get '/most_searcheds' => 'searched_terms#most_searched'
+  get '/most_searched' => 'searched_terms#most_searched'
 
 end
