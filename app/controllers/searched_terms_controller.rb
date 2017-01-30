56d3b676401794705284a560d80ea6f7af770cf2
MattPorto <matheus.pereira11@gmail.com>
2017-01-29 23:33:39 -0200

    update js and remove unecessary spaces on terms

--- app/controllers/searched_terms_controller.rb
+++ app/controllers/searched_terms_controller.rb
@@ -1,5 +1,6 @@
 class SearchedTermsController < ApplicationController
   def home
+    @last_search = session[:last_search]
   end
 
   def index
@@ -10,9 +11,10 @@ class SearchedTermsController < ApplicationController
     @searched_term = SearchedTerm.new
   end
   def create
-    @searched_term = SearchedTerm.find_by(term: params[:search])
+    session[:last_search] = search_param
+    @searched_term = SearchedTerm.find_by(term: search_param)
     unless @searched_term
-      @searched_term = SearchedTerm.new(term: params[:search])
+      @searched_term = SearchedTerm.new(term: search_param)
       @searched_term.save
     else
       @searched_term.increment! :popularity
@@ -22,6 +24,12 @@ class SearchedTermsController < ApplicationController
 
   def most_searched
     @searched_terms = SearchedTerm.all.order(popularity: :desc).select {|n| n.popularity > 1}
+  end
+
+  private
+
+  def search_param
+    params[:search].strip
   end
 
 end
